039eb5ce26f880d23124b8fd77171caec70a333c
Rishabh <rishabhmittal200@gmail.com>
2018-02-18 17:41:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,12 +12,29 @@ button.onclick = function () {
                  var span = document.getElementById('count');
                  span.innerHTML = counter.toString();
             }
-        } 
+        }
         // NOt done yet
     };
 //make a request
 request.open('GET','http://rishabhmittal200.imad.hasura-app.io/counter', true);
 request.send(null);
 
- 
+
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = doucument.getElementById('submit_btn');
+submt.onclick = function (){
+  //Make a request to the server and send the name
+  //Capture a list of name and render it.
+
+  var names =['name1','name2','name3','name4'];
+  var list ='';
+  for(var i=0;i<names.length;i++){
+    list +='<li>' + names[i] + '<li>'  
+
+  }
+  var ul = document.getElementsById('namelist')
+  ul.innerHTML = list;
+}
